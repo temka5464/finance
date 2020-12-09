53e0da02ef17dec5df38cee9baad5797bef9aa10
Temka <temuulenb26@gmail.com>
2020-12-09 14:26:41 +0800

    Orlogo zarlagyg web huudsan haragduuldag bolgono

--- app.js
+++ app.js
@@ -19,6 +19,28 @@ var uiController = (function () {
     getDomStrings: function () {
       return domStrings;
     },
+
+    addListItem: function (item, type) {
+      //Orlogo zarlagyn elementiig aguulsan html beltgene.
+      var html, list;
+      if (type === "inc") {
+        list = ".income__list";
+        html =
+          '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else {
+        list = ".expenses__list";
+        html =
+          '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">- $$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+
+      //ter html dotroo orlogo zarlagyn utguudiig REPLACE ashiglaj uurchilj ugnu
+      html = html.replace("%id%", item.id);
+      html = html.replace("$$DESCRIPTION$$", item.description);
+      html = html.replace("$$VALUE$$", item.value);
+
+      //beltgesen HTML-ee DOM ruu hiij ugnu
+      document.querySelector(list).insertAdjacentHTML("beforeend", html);
+    },
   };
 })();
 
@@ -61,6 +83,8 @@ var financeController = (function () {
       }
 
       data.items[type].push(item);
+
+      return item;
     },
     seeData: function () {
       return data;
@@ -73,9 +97,17 @@ var appController = (function (uiController, financeController) {
   var ctrlAddItem = function () {
     //1. Oruulah ugugdliig delgetsnees olj avna.
     var input = uiController.getInput();
+
     //2. Olson ugugdluudiig sanhuugiin controllert damjuulj tend hadgalna
-    financeController.addItem(input.type, input.description, input.value);
+    var item = financeController.addItem(
+      input.type,
+      input.description,
+      input.value
+    );
+
     //3. Olson ugugdluudiig web deer tohiroh gazart ni gargana
+    uiController.addListItem(item, input.type);
+
     //4. Tusviig tootsoolno
     //5. Etssiin uldegdel, tootsoog delgetsend gargana
   };
